4cf0c28e010c2eea94cd6b19fe1bb1cc135f588d
pinieb <petebiencourt@gmail.com>
2015-02-07 11:19:11 -0500

    Issue #19

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -50,29 +50,46 @@ namespace HexGame
         {
             // Create players
             this.Players = new List<PlayerController>();
-            this.Players.Add(new PlayerController(0));
-            this.Players[0].Color = Color.red;
             this.Players.Add(new PlayerController(1));
+            this.Players[0].Color = Color.red;
+            this.Players.Add(new PlayerController(2));
             this.Players[1].Color = Color.cyan;
 
             // Initialize Turn Controller
             this.TurnController.Initialize(this.Players);
 
             // Spawn units
-            var spawnCoord = new CellCoordinate(1, 2);
-            this.BoardController.SpawnUnit(UnitType.Cube, spawnCoord, this.Players[0]);
+            // Player 1
+            // Cubes
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(-3, -2), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(-3, -1), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(-3, 0), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(-4, 1), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(-5, 2), this.Players[0]);
 
-            spawnCoord = new CellCoordinate(1, 3);
-            this.BoardController.SpawnUnit(UnitType.Cube, spawnCoord, this.Players[0]);
+            // Arrows
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(-4, -1), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(-4, 0), this.Players[0]);
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(-5, 1), this.Players[0]);
 
-            spawnCoord = new CellCoordinate(2, 2);
-            this.BoardController.SpawnUnit(UnitType.Cube, spawnCoord, this.Players[1]);
+            // Pyramid
+            this.BoardController.SpawnUnit(UnitType.Pyramid, new CellCoordinate(-5, 0), this.Players[0]);
 
-            spawnCoord = new CellCoordinate(0, 0);
-            this.BoardController.SpawnUnit(UnitType.Pyramid, spawnCoord, this.Players[1]);
+            // Player 2
+            // Cubes
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(3, 2), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(3, 1), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(3, 0), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(4, -1), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Cube, new CellCoordinate(5, -2), this.Players[1]);
 
-            spawnCoord = new CellCoordinate(1, 0);
-            this.BoardController.SpawnUnit(UnitType.Arrow, spawnCoord, this.Players[1]);
+            // Arrows
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(4, 1), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(4, 0), this.Players[1]);
+            this.BoardController.SpawnUnit(UnitType.Arrow, new CellCoordinate(5, -1), this.Players[1]);
+
+            // Pyramid
+            this.BoardController.SpawnUnit(UnitType.Pyramid, new CellCoordinate(5, 0), this.Players[1]);
         }
     }
 }
